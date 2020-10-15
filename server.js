cb708d05e547bdba3f79d026527b094a53c9707e
tomC <tc13270@my.bristol.ac.uk>
2020-10-15 16:12:51 +0100

    Added presence updates by clientId and connectionId

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const amqp = require('amqplib/callback_api');
 const Ably = require('ably');
 const ServerPort = 3000;
 
-let currentState = {};
+let currentStateByChannel = {};
+let currentStateByClientId = {};
+let currentStateByConnectionId = {};
 let updatingLoop;
 let amqpConnection;
 
@@ -16,25 +18,25 @@ if (process.env.QUEUE_NAME == undefined) {
 }
 const queueEndpoint = "us-east-1-a-queue.ably.io:5671/shared";
 
-
 /* Ably details */
 const rest = new Ably.Rest({ key: apiKey });
 const realtime = new Ably.Realtime({ key: apiKey });
-
 let channelNamespace;
 if (process.env.NAMESPACE_REGEX === undefined) {
   channelNamespace = /^presence:.*/;
 } else {
   channelNamespace = new RegExp(process.env.NAMESPACE_REGEX);
 }
-const publishChannel = rest.channels.get('presence-updates');
+const publishByChannelChannel = rest.channels.get('presencebatch:by-channel');
+const publishByClientIdChannel = rest.channels.get('presencebatch:by-clientId');
+const publishByConnectionIdChannel = rest.channels.get('presencebatch:by-connectionId');
+
 const channelOptions = {
   params: {
-    rewind: '1'
+    rewind: 1
   }
 };
-const commandChannel = realtime.channels.get('presence-command', channelOptions);
-
+const commandChannel = realtime.channels.get('presencebatch:commands', channelOptions);
 commandChannel.subscribe(function(message) {
   if (message.name == 'update' && message.data == 'start') {
     startUpdates();
@@ -54,7 +56,9 @@ function stopUpdates() {
 }
 
 function updateAbly() {
-  publishChannel.publish('presence-update', currentState);
+  publishByChannelChannel.publish('presence-update', currentStateByChannel);
+  publishByClientIdChannel.publish('presence-update', currentStateByClientId);
+  publishByConnectionIdChannel.publish('presence-update', currentStateByConnectionId);
 }
 
 function consumeFromAbly() {
@@ -83,18 +87,15 @@ function consumeFromAbly() {
         let messages = decodedEnvelope.presence;
 
         messages.forEach(function(message) {
-          let action = message.action;
           let clientId = message.clientId;
-          if (currentState[currentChannel][clientId] != undefined &&
-              message.timestamp <= currentState[currentChannel][clientId].timestamp) {
-                /* Old message, don't do anything with it */
-          } else if (action == 3) {
-            /* 3 is the leave event, so remove them from the channel */
-            delete currentState[currentChannel][clientId];
-          } else if (action == 2) {
-            /* 2 is the enter event, so add them from the channel */
-            currentState[currentChannel][clientId] = message;
-          }
+          let connectionId = message.connectionId;
+
+          /* Update presence by channel object */
+          updateObject(currentStateByChannel, currentChannel, clientId, connectionId, message);
+          /* Update presence by clientID object */
+          updateObject(currentStateByClientId, clientId, currentChannel, connectionId, message);
+          /* Update presence by connectionId object */
+          updateObject(currentStateByConnectionId, connectionId, clientId, currentChannel, message);
         });
 
         /* Remove message from queue */
@@ -105,6 +106,34 @@ function consumeFromAbly() {
     conn.on('error', function(err) { console.error('worker:', 'Connection error!', err); });
   });
 };
+
+function updateObject(obj, param1, param2, param3, message) {
+  let action = message.action;
+
+  if (obj[param1] == undefined) {
+    obj[param1] = {};
+  }
+  if (obj[param1][param2] == undefined) {
+    obj[param1][param2] = {};
+  }
+
+  if (obj[param1][param2][param3] != undefined &&
+      message.timestamp <= obj[param1][param2][param3].timestamp) {
+    /* Old message, don't do anything with it */
+  } else if (action == 3) {
+    /* 3 is the leave event, so remove them from the channel */
+    delete obj[param1][param2][param3];
+    if (Object.keys(obj[param1][param2]).length === 0) {
+      delete obj[param1][param2];
+    }
+    if (Object.keys(obj[param1]).length === 0) {
+      delete obj[param1];
+    }
+  } else if (action == 2) {
+    /* 2 is the enter event, so add them to the channel */
+    obj[param1][param2][param3] = message;
+  }
+}
 
 function initializeState() {
   /* Make a request for all currently active channels */
@@ -126,10 +155,12 @@ function initializeState() {
 
 function getPresenceSets(response) {
   let channelsToCheck = "";
+  let firstElementAdded = false;
   for (let i = 0; i < response.items.length; i++) {
     let channelId = response.items[i].channelId;
     if (response.items[i].status.isActive && channelId.match(channelNamespace)) {
-      if (i == 0) {
+      if (!firstElementAdded) {
+        firstElementAdded = true;
         channelsToCheck = channelId;
       } else {
         channelsToCheck += "," + channelId;
@@ -159,10 +190,30 @@ function getPresenceSets(response) {
 
 function updateCurrentState(presenceSet) {
   for (let i = 0; i < presenceSet.length; i++) {
-    currentState[presenceSet[i].channel] = {};
+    currentStateByChannel[presenceSet[i].channel] = {};
+
     for (let j = 0; j < presenceSet[i].presence.length; j++) {
-      /* We may want to make a unique ID of connID = clientID */
-      currentState[presenceSet[i].channel][presenceSet[i].presence[j].clientId] = presenceSet[i].presence[j];
+      let message = presenceSet[i].presence[j];
+      let clientId = presenceSet[i].presence[j].clientId;
+      let connectionId = presenceSet[i].presence[j].connectionId;
+      let channel = presenceSet[i].channel;
+
+      /* Add the presence message to the object sorted by channel */
+      createObjectField(currentStateByChannel, channel, clientId, connectionId, message);
+      /* Add the presence message to the object sorted by clientId */
+      createObjectField(currentStateByClientId, clientId, channel, connectionId, message);
+      /* Add the presence message to the object sorted by connectionId */
+      createObjectField(currentStateByConnectionId, connectionId, clientId, channel, message);
     }
+  }
+
+  function createObjectField(obj, param1, param2, param3, message) {
+      if (obj[param1] === undefined) {
+        obj[param1] = {};
+      }
+      if (obj[param1][param2] === undefined) {
+        obj[param1][param2] = {};
+      }
+      obj[param1][param2][param3] = message;
   }
 }
